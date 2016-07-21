73db2390bbd695135649ad0d56ba9bc538e6facc
Elliot Tormey <elliot.tormey@tapadoo.com>
2016-07-21 15:23:11 +0100

    update environmentSetup.sh, remove gradle.properties value

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 function copyEnvVarsToGradleProperties {
     GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
     export GRADLE_PROPERTIES
@@ -10,6 +11,6 @@ function copyEnvVarsToGradleProperties {
         touch $GRADLE_PROPERTIES
  
         echo "Writing TEST_API_KEY to gradle.properties..."
-        echo "TEST_API_KEY=$TEST_API_KEY_ENV_VAR" >> $GRADLE_PROPERTIES
+        echo "TEST_VALUE=$TEST_VALUE_ENV_VAR" >> $GRADLE_PROPERTIES
     fi
 }
